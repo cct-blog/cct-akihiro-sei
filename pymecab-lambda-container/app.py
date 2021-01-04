2b6a698c15cbce27a97c02a1bba067363f62cc44
cct-akihiro-sei <akihiro.sei@cct-inc.co.jp>
2021-01-04 17:53:04 +0900

    add code pymecab-lambda-container for new post.

---
+++ pymecab-lambda-container/app.py
@@ -0,0 +1,27 @@
+import json
+import MeCab
+
+neologd_tagger = MeCab.Tagger(
+    '-O wakati '
+    '-r /dev/null '
+    '-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+
+
+def lambda_handler(event, context):
+    node = neologd_tagger.parseToNode(event['body'])
+
+    result = []
+    while node:
+        if not node.feature.startswith('BOS/EOS'):
+            result.append(node.feature)
+        node = node.next
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps(result),
+        "isBase64Encoded": False
+    }
